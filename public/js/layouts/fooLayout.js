ec5e1b61acb4a6e0175f69514c3945911bea70ce
Lin Jun <linjun110@gmail.com>
2014-06-04 10:59:09 +0800

    Second commit

---
+++ public/js/layouts/fooLayout.js
@@ -0,0 +1,31 @@
+define(['marionette', 'text!templates/fooLayoutTemplate.html', 'views/booksCompositeView', 'models/booksCollection', 'views/foodsCompositeView', 'models/foodsCollection'],function(marionette, Template, BooksCompositeView, BooksCollection, FoodsCompositeView, FoodsCollection){
+//define(['marionette', 'text!templates/fooLayoutTemplate.html', 'views/booksCompositeView', 'models/booksCollection', 'views/userView', 'models/userModel'],function(marionette, Template, BooksCompositeView, BooksCollection, UserView, UserModel){
+	var layout = Backbone.Marionette.Layout.extend({
+		template: Template,
+		initialize: function(){
+			this.region_1.on("show", function(view){
+				//console.log("fooLayout.foo_layout_region_1 show:",view);
+			});
+			this.region_1.on("close", function(view){
+				//console.log("fooLayout.foo_layout_region_1 close:",view);
+			});
+			this.region_2.on("show", function(view){
+				//console.log("fooLayout.foo_layout_region_2 show:",view);
+			});
+			this.region_2.on("close", function(view){
+				//console.log("fooLayout.foo_layout_region_2 close:",view);
+			});
+		},
+		regions: {
+			region_1: "#foo_layout_region_1",
+			region_2: "#foo_layout_region_2"
+		},
+		onShow: function () {
+			this.region_1.show(new BooksCompositeView({collection: new BooksCollection()}));
+			this.region_2.show(new FoodsCompositeView({collection: new FoodsCollection()}));
+
+			//this.region_2.show(new UserView({model: new UserModel({_id: 0})}));
+		}
+	});
+	return layout;
+});
